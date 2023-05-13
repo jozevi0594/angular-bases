a725e857533cc1b2459c2e0e9f89fa92ef804a6e
jozevi0594 <jozevi0594@gmail.com>
2023-05-12 20:32:03 -0500

    seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
+import { heroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    heroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
